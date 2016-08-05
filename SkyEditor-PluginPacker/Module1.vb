5e4479cb4b196aaf009b3457fcf6c7cababdb9d7
Evan Dixon <evanldixon@live.com>
2016-08-05 15:01:26 -0500

    Added experimental code to pack plugins

--- SkyEditor-PluginPacker/Module1.vb
+++ SkyEditor-PluginPacker/Module1.vb
@@ -1,7 +1,44 @@
-﻿Module Module1
+﻿Imports System.IO
+Imports System.Reflection
+Imports SkyEditor.Core
+Imports SkyEditor.Core.Extensions
+Imports SkyEditor.Core.Windows.Utilities
+
+Module Module1
 
     Sub Main()
+        Dim args = Environment.GetCommandLineArgs
+        If args.Length > 2 Then
+            Console.WriteLine("Sky Editor Plugin Packer v" & Assembly.GetExecutingAssembly.GetName.Version.ToString(3))
+            Console.WriteLine()
 
+            Using manager As New PluginManager
+                Dim core As New PluginPackPluginCore(args(1))
+                Console.WriteLine("Loading core...")
+                manager.LoadCore(core).Wait()
+
+                Console.WriteLine("Setting up...")
+                Dim dependant = core.DependantPlugin
+                Dim a = dependant.GetType.Assembly
+                Dim info As New ExtensionInfo
+                info.Name = dependant.PluginName
+                'Todo: set description
+                info.Author = dependant.PluginAuthor
+                info.Version = a.GetName.Version.ToString(3)
+
+                If Not Directory.Exists(Path.GetDirectoryName(args(2))) Then
+                    Directory.CreateDirectory(Path.GetDirectoryName(args(2)))
+                End If
+
+                Console.WriteLine("Packing plugin...")
+                RedistributionHelpers.PackPlugins({dependant}, args(2), info, manager).Wait()
+
+                Console.WriteLine("Done!")
+            End Using
+        Else
+            Console.WriteLine("Usage: SkyEditor-PluginPacker.exe <AssemblyPath> <TargetZipPath>")
+            Environment.Exit(1)
+        End If
     End Sub
 
 End Module
---
+++ SkyEditor-PluginPacker/PluginPackPluginCore.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Reflection
+Imports SkyEditor.Core
+Imports SkyEditor.Core.Utilities
+
+Public Class PluginPackPluginCore
+    Inherits SkyEditor.Core.Windows.CoreMods.ConsoleCoreMod
+
+    Public Sub New(targetFilename As String)
+        MyBase.New
+        Me.TargetFilename = targetFilename
+    End Sub
+
+    Public Property TargetFilename As String
+
+    ''' <summary>
+    ''' The plugin located at <see cref="TargetFilename"/>.  Will be null until <see cref="Load(PluginManager)"/> is called.
+    ''' </summary>
+    Public Property DependantPlugin As SkyEditorPlugin
+
+
+    Public Overrides Sub Load(manager As PluginManager)
+        MyBase.Load(manager)
+
+        Dim targetAssembly = LoadAssembly(TargetFilename)
+        Dim firstPluginType = targetAssembly.GetTypes.Where(Function(x) ReflectionHelpers.IsOfType(x, GetType(SkyEditorPlugin).GetTypeInfo) AndAlso ReflectionHelpers.CanCreateInstance(x)).FirstOrDefault
+        If firstPluginType Is Nothing Then
+            Throw New Exception("Target assembly is not a valid Sky Editor plugin.")
+        Else
+            Dim plugin = ReflectionHelpers.CreateInstance(firstPluginType)
+            manager.LoadRequiredPlugin(plugin, Me)
+            DependantPlugin = plugin
+        End If
+    End Sub
+
+End Class
