0ca91c0d1796ea27e11a4c4836a6ec46ff527563
Evan Dixon <evanldixon@live.com>
2016-08-16 10:09:09 -0500

    NWC: Modeled what needs to be done

--- SkyEditor-PluginPacker/Module1.vb
+++ SkyEditor-PluginPacker/Module1.vb
@@ -12,29 +12,36 @@ Module Module1
             Console.WriteLine("Sky Editor Plugin Packer v" & Assembly.GetExecutingAssembly.GetName.Version.ToString(3))
             Console.WriteLine()
 
-            Using manager As New PluginManager
-                Dim core As New PluginPackPluginCore(args(1))
-                Console.WriteLine("Loading core...")
-                manager.LoadCore(core).Wait()
+            'Todo: create info.skyext with relevant info (possibly grabbed using reflection)
+            'Todo: if info.skyext exists, update relevant info (like extension type)
+            'Todo: zip the directory, including info.skyext
 
-                Console.WriteLine("Setting up...")
-                Dim dependant = core.DependantPlugin
-                Dim a = dependant.GetType.Assembly
-                Dim info As New ExtensionInfo
-                info.Name = dependant.PluginName
-                'Todo: set description
-                info.Author = dependant.PluginAuthor
-                info.Version = a.GetName.Version.ToString(3)
+            Throw New NotImplementedException
 
-                If Not Directory.Exists(Path.GetDirectoryName(args(2))) Then
-                    Directory.CreateDirectory(Path.GetDirectoryName(args(2)))
-                End If
+            ''Scrap this
+            'Using manager As New PluginManager
+            '    Dim core As New PluginPackPluginCore(args(1))
+            '    Console.WriteLine("Loading core...")
+            '    manager.LoadCore(core).Wait()
 
-                Console.WriteLine("Packing plugin...")
-                RedistributionHelpers.PackPlugins({dependant}, args(2), info, manager).Wait()
+            '    Console.WriteLine("Setting up...")
+            '    Dim dependant = core.DependantPlugin
+            '    Dim a = dependant.GetType.Assembly
+            '    Dim info As New ExtensionInfo
+            '    info.Name = dependant.PluginName
+            '    'Todo: set description
+            '    info.Author = dependant.PluginAuthor
+            '    info.Version = a.GetName.Version.ToString(3)
 
-                Console.WriteLine("Done!")
-            End Using
+            '    If Not Directory.Exists(Path.GetDirectoryName(args(2))) Then
+            '        Directory.CreateDirectory(Path.GetDirectoryName(args(2)))
+            '    End If
+
+            '    Console.WriteLine("Packing plugin...")
+            '    RedistributionHelpers.PackPlugins({dependant}, args(2), info, manager).Wait()
+
+            '    Console.WriteLine("Done!")
+            'End Using
         Else
             Console.WriteLine("Usage: SkyEditor-PluginPacker.exe <AssemblyPath> <TargetZipPath>")
             Environment.Exit(1)
